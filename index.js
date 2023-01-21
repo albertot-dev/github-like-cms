aed304d8fb2b70377f14f57cbc640e0f61068e59
albertot_dev <atejero.dam@gmail.com>
2023-01-21 18:53:01 +0100

    first version

---
+++ index.js
@@ -0,0 +1,15 @@
+import {
+  getLastPosts,
+  generatePaths,
+  generatePages,
+  getPostsPagination
+} from './lib'
+
+const glcms = {
+  getLastPosts,
+  generatePaths,
+  generatePages,
+  getPostsPagination
+}
+
+export default glcms
